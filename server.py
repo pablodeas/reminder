ee202a3959b2542bb5f689822cac3922e45213fd
Pablo Andrade <pablodeas@gmail.com>
2026-01-06 21:47:20 -0300

    change port

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request
+from database import Session, init_db
+from models import Reminder
 
+init_db()
 
 app = Flask(__name__)
 
@@ -8,6 +11,21 @@ app = Flask(__name__)
 def super():
     return jsonify(message="It's working"), 200
 
+@app.route('/list')
+def list():
+    session = Session()
+    rem = session.query(Reminder).order_by(Reminder.event_date.asc()).all()
+    
+    reminders_list = []
+    for reminder in rem:
+        reminders_list.append({
+            "event_date": str(reminder.event_date),
+            "message": reminder.message
+        })
+    
+    return jsonify(reminders=reminders_list), 200
+    session.close()
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5050)
